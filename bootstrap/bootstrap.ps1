98c217106af0449d4e2d6f94e66e3ca81fdfca6e
James Dawson <james.dawson@endjin.com>
2021-08-26 18:48:01 +0100

    More bootstrap tweaks

--- bootstrap/bootstrap.ps1
+++ bootstrap/bootstrap.ps1
@@ -1,20 +1,24 @@
 # Install BoxStarter & Chocolatey
-function _log([string] $msg) { $msg >> c:/WindowsAzure/vm-bootstrap.log }
+function _log([string] $msg) { "### $(Get-Date) $msg" >> c:/WindowsAzure/vm-bootstrap.log }
 
 _log "################################"
 _log "################################"
-_log (Get-Date)
 
 _log "### Setting ExecutionPolicy..."
 Set-ExecutionPolicy RemoteSigned -Force
 
-_log "### Bootstrapping BoxStarter..."
+_log "Bootstrapping BoxStarter..."
 . { Invoke-WebRequest -UseBasicParsing https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
-_log "### BoxStarter bootstrap complete"
+_log "BoxStarter bootstrap complete"
 
 $starterPackage = 'https://raw.githubusercontent.com/endjin/endjin-workstation-setup/feature/choco/bootstrap/bootstrap-boxstarter-script.txt'
-_log "### Running bootstrap script..."
+_log "Running bootstrap script..."
 Install-BoxStarterPackage -PackageName $starterPackage -DisableReboots
+_log "Bootstrap script complete"
 
-_log "### Rebooting..."
+_log "Installing WSL..."
+wsl --install >> c:/WindowsAzure/vm-bootstrap.log
+_log "WSL install complete"
+
+_log "Rebooting..."
 shutdown /r /t 0
