dda7620ddded23ed81d643ff4d0de36b14a9c19d
James Dawson <james.dawson@endjin.com>
2021-08-27 00:44:24 +0100

    More bootstrap debugging

--- bootstrap/bootstrap.ps1
+++ bootstrap/bootstrap.ps1
@@ -11,14 +11,23 @@ _log "Bootstrapping BoxStarter..."
 . { Invoke-WebRequest -UseBasicParsing https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
 _log "BoxStarter bootstrap complete"
 
-$starterPackage = 'https://raw.githubusercontent.com/endjin/endjin-workstation-setup/feature/choco/bootstrap/bootstrap-boxstarter-script.txt'
+#$starterPackage = 'https://raw.githubusercontent.com/endjin/endjin-workstation-setup/feature/choco/bootstrap/bootstrap-boxstarter-script.txt'
 _log "Running bootstrap script..."
-Install-BoxStarterPackage -PackageName $starterPackage -DisableReboots
+#Install-BoxStarterPackage -PackageName $starterPackage -DisableReboots
+# Workaround issue with TEMP path getting polluted due to Boxstarter issue: https://github.com/chocolatey/boxstarter/issues/241
+$cachePath = (Resolve-Path ([IO.Path]::Combine($env:USERPROFILE, 'AppData', 'Local', 'Temp', 'chocolatey'))).Path
+choco install --nocolor --cacheLocation=$cachePath -y powershell-core 2>&1 >>  c:/WindowsAzure/vm-bootstrap.log
+choco install --nocolor --cacheLocation=$cachePath -y git 2>&1 >>  c:/WindowsAzure/vm-bootstrap.log
+choco install --nocolor --cacheLocation=$cachePath -y vscode 2>&1 >>  c:/WindowsAzure/vm-bootstrap.log
 _log "Bootstrap script complete"
+
+_log "Apply Windows Updates..."
+Install-WindowsUpdate -Criteria "IsInstalled=0 and Type='Software'" -AcceptEula -SuppressReboot
+_log "Windows Updates complete"
 
 _log "Installing WSL..."
 wsl --install >> c:/WindowsAzure/vm-bootstrap.log
 _log "WSL install complete"
 
-_log "Rebooting..."
-shutdown /r /t 0
+#_log "Rebooting..."
+#shutdown /r /t 0
