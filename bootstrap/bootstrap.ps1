d6461c069c6c7bc1177d02c67a7eb652d1d298c4
James Dawson <james.dawson@endjin.com>
2021-08-26 18:24:32 +0100

    Add some of own logging to try and figure out why the bootstrap doesn't appear to run

--- bootstrap/bootstrap.ps1
+++ bootstrap/bootstrap.ps1
@@ -1,8 +1,20 @@
 # Install BoxStarter & Chocolatey
+function _log([string] $msg) { $msg >> c:/WindowsAzure/vm-bootstrap.log }
+
+_log "################################"
+_log "################################"
+_log (Get-Date)
+
+_log "### Setting ExecutionPolicy..."
 Set-ExecutionPolicy RemoteSigned -Force
+
+_log "### Bootstrapping BoxStarter..."
 . { Invoke-WebRequest -UseBasicParsing https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
+_log "### BoxStarter bootstrap complete"
 
 $starterPackage = 'https://raw.githubusercontent.com/endjin/endjin-workstation-setup/feature/choco/bootstrap/bootstrap-boxstarter-script.txt'
+_log "### Running bootstrap script..."
 Install-BoxStarterPackage -PackageName $starterPackage -DisableReboots
 
+_log "### Rebooting..."
 shutdown /r /t 0
