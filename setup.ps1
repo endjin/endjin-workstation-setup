19e21a0e31fca8cf572dd202491d35fe2c90ca89
James Dawson <james.dawson@endjin.com>
2021-08-27 11:56:46 +0100

    Added reboot reminder message to setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,9 @@
 $here = Split-Path -Parent $PSCommandPath
 
 Install-BoxStarterPackage -PackageName "$here/setup-boxstarter-script.txt" -DisableReboots
+
+Write-Host "`n*******************************************************************"
+Write-Host "*** If this is the first time you have run this script, you will"
+Write-Host "*** need to reboot your machine to complete the WSL installation"
+Write-Host "*** before Docker Desktop will work properly."
+Write-Host "*******************************************************************`n"
