c3ae9e5b3f2d35fcf0b2477368b9c372ec4cae1d
James Dawson <james.dawson@endjin.com>
2021-08-27 09:49:29 +0100

    strip back bootstrap

--- bootstrap/bootstrap.ps1
+++ bootstrap/bootstrap.ps1
@@ -4,8 +4,6 @@ function _log([string] $msg) { "### $(Get-Date) $msg" >> c:/WindowsAzure/vm-boot
 _log "################################"
 _log "################################"
 
-Read-Host "pause"
-
 _log "### Setting ExecutionPolicy..."
 Set-ExecutionPolicy RemoteSigned -Force
 
@@ -18,24 +16,24 @@ _log "Running bootstrap script..."
 #Install-BoxStarterPackage -PackageName $starterPackage -DisableReboots
 # Workaround issue with TEMP path getting polluted due to Boxstarter issue: https://github.com/chocolatey/boxstarter/issues/241
 $cachePath = (Resolve-Path ([IO.Path]::Combine($env:USERPROFILE, 'AppData', 'Local', 'Temp', 'chocolatey'))).Path
-choco install --nocolor --cacheLocation=$cachePath -y powershell-core 2>&1 >>  c:/WindowsAzure/vm-bootstrap.log
+# choco install --nocolor --cacheLocation=$cachePath -y powershell-core 2>&1 >>  c:/WindowsAzure/vm-bootstrap.log
 choco install --nocolor --cacheLocation=$cachePath -y git 2>&1 >>  c:/WindowsAzure/vm-bootstrap.log
-choco install --nocolor --cacheLocation=$cachePath -y vscode 2>&1 >>  c:/WindowsAzure/vm-bootstrap.log
+# choco install --nocolor --cacheLocation=$cachePath -y vscode 2>&1 >>  c:/WindowsAzure/vm-bootstrap.log
 _log "Bootstrap script complete"
 
-_log "Apply Windows Updates..."
-Install-WindowsUpdate -Criteria "IsInstalled=0 and Type='Software'" -AcceptEula -SuppressReboot
-_log "Windows Updates complete"
+# _log "Apply Windows Updates..."
+# Install-WindowsUpdate -Criteria "IsInstalled=0 and Type='Software'" -AcceptEula -SuppressReboot
+# _log "Windows Updates complete"
 
 _log "Installing WSL..."
 #wsl --install >> c:/WindowsAzure/vm-bootstrap.log
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart >> c:/WindowsAzure/vm-bootstrap.log
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart >> c:/WindowsAzure/vm-bootstrap.log
-Invoke-WebRequest -Uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" -OutFile ./wsl_update_x64.msi
-& msiexec.exe /i $((Resolve-Path ./wsl_update_x64.msi).Path) /passive /l*v c:/WindowsAzure/wsl2-install.log | Out-Null
-"MSIExec code: $LASTEXITCODE" >> c:/WindowsAzure/vm-bootstrap.log
+# dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart >> c:/WindowsAzure/vm-bootstrap.log
+# dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart >> c:/WindowsAzure/vm-bootstrap.log
+# Invoke-WebRequest -Uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" -OutFile ./wsl_update_x64.msi
+# & msiexec.exe /i $((Resolve-Path ./wsl_update_x64.msi).Path) /passive /l*v c:/WindowsAzure/wsl2-install.log | Out-Null
+# "MSIExec code: $LASTEXITCODE" >> c:/WindowsAzure/vm-bootstrap.log
 #wsl --set-default-version 2 >> c:/WindowsAzure/vm-bootstrap.log
-_log "WSL install complete"
+# _log "WSL install complete"
 
 #_log "Rebooting..."
-shutdown /r /t 0
+# shutdown /r /t 0
