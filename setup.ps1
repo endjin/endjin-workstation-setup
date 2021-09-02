61b1b963c2bd714311b7642b9fc0fa83f29b2c9c
James Dawson <james.dawson@endjin.com>
2021-09-02 17:06:21 +0100

    Remove BoxStarter dependency from setup.ps1
    This complicates the 'existing machine' scenario and currently adds little value.

--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,18 @@
 $here = Split-Path -Parent $PSCommandPath
 
-Install-BoxStarterPackage -PackageName "$here/setup-boxstarter-script.txt" -DisableReboots
+Write-Host "`n*******************************************************************"
+Write-Host "*** Have you followed the steps for installing the"
+Write-Host "*** Windows Subsystem for Linux (aka WSL)?"
+Write-Host "*** Link: https://github.com/endjin/endjin-workstation-setup/blob/feature/choco/docs/setup_vm.md#installing-windows-subsystem-for-linux"
+
+# Workaround issue with TEMP path getting polluted due to Boxstarter issue: https://github.com/chocolatey/boxstarter/issues/241
+$cachePath = (Resolve-Path ([IO.Path]::Combine($env:USERPROFILE, 'AppData', 'Local', 'Temp', 'chocolatey'))).Path
+
+# Install/upgrade packages using the configuration file
+choco install --cacheLocation=$cachePath -y ./setup-chocolatey-packages.config
 
 Write-Host "`n*******************************************************************"
 Write-Host "*** If this is the first time you have run this script, you will"
-Write-Host "*** need to reboot your machine to complete the WSL installation"
-Write-Host "*** before Docker Desktop will work properly."
+Write-Host "*** need to log out and log back in to your machine before"
+Write-Host "*** Docker Desktop will work properly."
 Write-Host "*******************************************************************`n"
