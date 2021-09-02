58b047209f79ebfea4c3779957aef715b597c1c3
James Dawson <james.dawson@endjin.com>
2021-09-02 13:48:40 +0100

    Disable WSL automation

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 $here = Split-Path -Parent $PSCommandPath
 
-Install-BoxStarterPackage -PackageName "$here/setup-boxstarter-script.txt"
+Install-BoxStarterPackage -PackageName "$here/setup-boxstarter-script.txt" -DisableReboots
 
 Write-Host "`n*******************************************************************"
 Write-Host "*** If this is the first time you have run this script, you will"
