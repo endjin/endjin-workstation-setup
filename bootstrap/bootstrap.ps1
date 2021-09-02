d7dec5f27df804f5dd858adf4f588e83ad878ce3
James Dawson <james.dawson@endjin.com>
2021-09-02 10:48:49 +0100

    Tidy-up and another tweak to try and get WSL installed properly before installing docker

--- bootstrap/bootstrap.ps1
+++ bootstrap/bootstrap.ps1
@@ -11,25 +11,13 @@ _log "Bootstrapping BoxStarter..."
 . { Invoke-WebRequest -UseBasicParsing https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
 _log "BoxStarter bootstrap complete"
 
-#$starterPackage = 'https://raw.githubusercontent.com/endjin/endjin-workstation-setup/feature/choco/bootstrap/bootstrap-boxstarter-script.txt'
 _log "Running bootstrap script..."
-#Install-BoxStarterPackage -PackageName $starterPackage -DisableReboots
 # Workaround issue with TEMP path getting polluted due to Boxstarter issue: https://github.com/chocolatey/boxstarter/issues/241
 $cachePath = (Resolve-Path ([IO.Path]::Combine($env:USERPROFILE, 'AppData', 'Local', 'Temp', 'chocolatey'))).Path
-# choco install --nocolor --cacheLocation=$cachePath -y powershell-core 2>&1 >>  c:/WindowsAzure/vm-bootstrap.log
-choco install --nocolor --cacheLocation=$cachePath -y git 2>&1 >>  c:/WindowsAzure/vm-bootstrap.log
-# choco install --nocolor --cacheLocation=$cachePath -y vscode 2>&1 >>  c:/WindowsAzure/vm-bootstrap.log
-_log "Bootstrap script complete"
 
-#_log "Installing WSL..."
-#wsl --install >> c:/WindowsAzure/vm-bootstrap.log
-# dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart >> c:/WindowsAzure/vm-bootstrap.log
-# dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart >> c:/WindowsAzure/vm-bootstrap.log
-# Invoke-WebRequest -Uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" -OutFile ./wsl_update_x64.msi
-# & msiexec.exe /i $((Resolve-Path ./wsl_update_x64.msi).Path) /passive /l*v c:/WindowsAzure/wsl2-install.log | Out-Null
-# "MSIExec code: $LASTEXITCODE" >> c:/WindowsAzure/vm-bootstrap.log
-#wsl --set-default-version 2 >> c:/WindowsAzure/vm-bootstrap.log
-#_log "WSL install complete"
+# This script is run in the system context (rather than as a logged-in user), so we only install the minimum pre-requisites
+choco install --nocolor --cacheLocation=$cachePath -y git 2>&1 >>  c:/WindowsAzure/vm-bootstrap.log     # install git
+_log "Bootstrap script complete"
 
 _log "Apply Windows Updates..."
 Install-WindowsUpdate -Criteria "IsInstalled=0 and Type='Software'" -AcceptEula -SuppressReboot
